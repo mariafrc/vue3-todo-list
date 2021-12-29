7e5f75a968a9a0c95df6dacd641a9f481ef0d90a
Franck Bemaria <fb@alternateeve.com>
2021-12-29 17:03:14 +0300

    feat: quasar integration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from "vue";
+import { Quasar } from 'quasar';
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/dist/quasar.css'
+
 import App from "./App.vue";
 import store from "./store/index";
 
 const app = createApp(App);
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
 app.use(store);
 app.mount("#app");
